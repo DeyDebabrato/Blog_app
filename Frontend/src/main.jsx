08e9f67fe9f2b80c68af5c23cace2f80c4860e45
Debabrato Dey <125475821+DeyDebabrato@users.noreply.github.com>
2024-04-27 10:50:52 +0530

    Add files via upload

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={clientId}>
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
